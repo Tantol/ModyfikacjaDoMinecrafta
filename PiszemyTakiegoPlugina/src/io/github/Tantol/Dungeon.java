e248913030f20a567bdb9c9497b60e58038914c1
DoneQ <marcinrubach@gmail.com>
2017-03-26 20:20:52 +0200

    Dodana komenda

--- PiszemyTakiegoPlugina/src/io/github/Tantol/Dungeon.java
+++ PiszemyTakiegoPlugina/src/io/github/Tantol/Dungeon.java
@@ -1,5 +1,8 @@
 package io.github.Tantol;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Dungeon extends JavaPlugin {
@@ -11,5 +14,13 @@ public void onEnable() {
 }
 @Override
 public void onDisable() {
+}
+public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+    Player p = (Player) sender;
+    if(command.getName().equalsIgnoreCase("jebiecimatke")){
+    	p.sendMessage("Funkcja Wprowadzona");
+    	return true;
+    }
+    else return false;
 }
 }
