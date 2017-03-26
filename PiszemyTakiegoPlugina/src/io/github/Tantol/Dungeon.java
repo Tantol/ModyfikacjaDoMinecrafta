036455444281c298d7d52d9a6460c6427dff9e2e
Tantol <tomando5218@gmail.com>
2017-03-26 19:55:22 +0200

    /init plugin

---
+++ PiszemyTakiegoPlugina/src/io/github/Tantol/Dungeon.java
@@ -0,0 +1,15 @@
+package io.github.Tantol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Dungeon extends JavaPlugin {
+
+
+
+@Override
+public void onEnable() {
+}
+@Override
+public void onDisable() {
+}
+}
