1ae97146203051f8a1378466fbae6d00be23969f
DoneQ <marcinrubach@gmail.com>
2017-03-28 02:36:44 +0200

    Chat

--- PiszemyTakiegoPlugina/src/io/github/Tantol/RpChat.java
+++ PiszemyTakiegoPlugina/src/io/github/Tantol/RpChat.java
@@ -12,45 +12,20 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class RpChat implements CommandExecutor, Listener {
 	private static ArrayList<ChatOb> chat = new ArrayList<ChatOb>();
-	
-//	private static ArrayList<Player> ooc = new ArrayList<Player>();
-/*	private static ArrayList<Player> ic = new ArrayList<Player>();
-	private static ArrayList<Player> shout = new ArrayList<Player>();
-	private static ArrayList<Player> whisper = new ArrayList<Player>();
-	private static ArrayList<Player> me = new ArrayList<Player>();
-	private static ArrayList<Player> narrator = new ArrayList<Player>();
-	private static ArrayList<Player> gnarrator = new ArrayList<Player>();*/
 	private static ArrayList<Player> rec = new ArrayList<Player>();
 
 	/*
 	 * Tutaj Poin¿ej Edytowaæ
 	 */
 
-	//private int ooc_range = 4000;
-	/*private int ic_range = 30;
-	private int shout_range = 60;
-	private int whisper_range = 5;
-	private int me_range = 30;
-	private int narrator_range = 300;
-	private int gnarrator_range = 4000;
-	private String ooc_str = ChatColor.RED + "[OOC]";
-	private String ic_str = ChatColor.GREEN + "[IC]";
-	private String shout_str = ChatColor.BLUE + "[SHOUT]";
-	private String whisper_str = ChatColor.GRAY + "[WHISPER]";
-	private String me_str = ChatColor.YELLOW + "";
-	private String narrator_str = ChatColor.AQUA + "[NARRATOR]";
-	private String gnarrator_str = ChatColor.AQUA + "[NARRATOR GLOBALNY]";
-	
-	*/
 	public RpChat(){
-		chat.add( new ChatOb("ic",30,ChatColor.RED + "[IC]", new ArrayList<Player>(),"msg"));
+		chat.add( new ChatOb("ic",30,ChatColor.GREEN + "[IC]", new ArrayList<Player>(),"msg"));
 		chat.add( new ChatOb("ooc",4000,ChatColor.RED + "[OOC]", new ArrayList<Player>(),"msg"));
-		chat.add( new ChatOb("w",5,ChatColor.RED + "[WHISPER]", new ArrayList<Player>(),"msg"));
-		chat.add( new ChatOb("s",60,ChatColor.RED + "[SHOUT]", new ArrayList<Player>(),"msg"));
+		chat.add( new ChatOb("w",5,ChatColor.GRAY + "[WHISPER]", new ArrayList<Player>(),"msg"));
+		chat.add( new ChatOb("s",60,ChatColor.BLUE + "[SHOUT]", new ArrayList<Player>(),"msg"));
 		chat.add( new ChatOb("me",30,ChatColor.YELLOW + "", new ArrayList<Player>(),"me"));
-		chat.add( new ChatOb("n",300,ChatColor.YELLOW + "[NARRATOR]", new ArrayList<Player>(),"nar"));
-		chat.add( new ChatOb("ng",4000,ChatColor.YELLOW + "[NARRATOR GLOBALNY]", new ArrayList<Player>(),"nar"));
-		
+		chat.add( new ChatOb("n",300,ChatColor.AQUA + "[NARRATOR]", new ArrayList<Player>(),"nar"));
+		chat.add( new ChatOb("ng",4000,ChatColor.AQUA + "[NARRATOR GLOBALNY]", new ArrayList<Player>(),"nar"));	
 	}
 	/*
 	 * Tego co pod tym lepiej nie tykaæ jak nie wiesz co i jak ^^
@@ -61,20 +36,7 @@ public class RpChat implements CommandExecutor, Listener {
 		for (int i = 0; i < strings.length; i++) {
 			msg += strings[i] + " ";
 		}
-	/*	if (cmnd.getName().equalsIgnoreCase("ooc") && strings.length < 1) {
-			// s.sendMessage("Debug: ooc");
-			ic.remove(s);
-			shout.remove(s);
-			whisper.remove(s);
-			me.remove(s);
-			narrator.remove(s);
-			gnarrator.remove(s);
-			//ooc.add(s);
-			for(int i =0 ; i<chat.size(); i++)
-			{
-				chat.get(i).getList().add(s);
-			}
-		}*/
+
 		int flaga = 0;
 		for(int i =0 ; i<chat.size(); i++)
 		{
@@ -89,14 +51,6 @@ public class RpChat implements CommandExecutor, Listener {
 			flaga=1;
 		}
 		if (cmnd.getName().equalsIgnoreCase(chat.get(i).getCmd()) && strings.length < 1) {
-			// s.sendMessage("Debug: ic");
-		/*	ooc.remove(s);
-			shout.remove(s);
-			whisper.remove(s);
-			me.remove(s);
-			narrator.remove(s);
-			gnarrator.remove(s);
-			ic.add(s);*/
 			for(int j =0 ; j<chat.size(); j++)
 			{
 				chat.get(j).getList().remove(s);
@@ -106,78 +60,6 @@ public class RpChat implements CommandExecutor, Listener {
 		}
 		}
 		flaga=0;
-		/*	if (cmnd.getName().equalsIgnoreCase("ic") && strings.length >= 1) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			message(ic_range, s, msg, ic_str);
-		}
-		if (cmnd.getName().equalsIgnoreCase("s") && strings.length < 1) {
-			// s.sendMessage("Debug: shout");
-			ic.remove(s);
-			ooc.remove(s);
-			whisper.remove(s);
-			me.remove(s);
-			narrator.remove(s);
-			gnarrator.remove(s);
-			shout.add(s);
-		}
-		if (cmnd.getName().equalsIgnoreCase("s") && strings.length >= 1) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			message(shout_range, s, msg, shout_str);
-		}
-		if (cmnd.getName().equalsIgnoreCase("w") && strings.length < 1) {
-			// s.sendMessage("Debug: whisper");
-			ic.remove(s);
-			shout.remove(s);
-			ooc.remove(s);
-			me.remove(s);
-			narrator.remove(s);
-			gnarrator.remove(s);
-			whisper.add(s);
-		}
-		if (cmnd.getName().equalsIgnoreCase("w") && strings.length >= 1) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			message(whisper_range, s, msg, whisper_str);
-		}
-		if (cmnd.getName().equalsIgnoreCase("n") && strings.length < 1) {
-			// s.sendMessage("Debug: whisper");
-			ic.remove(s);
-			shout.remove(s);
-			ooc.remove(s);
-			me.remove(s);
-			whisper.remove(s);
-			gnarrator.remove(s);
-			narrator.add(s);
-		}
-		if (cmnd.getName().equalsIgnoreCase("n") && strings.length >= 1) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			narrator(narrator_range, s, msg, narrator_str);
-		}
-		if (cmnd.getName().equalsIgnoreCase("ng") && strings.length < 1) {
-			// s.sendMessage("Debug: whisper");
-			ic.remove(s);
-			shout.remove(s);
-			ooc.remove(s);
-			me.remove(s);
-			narrator.remove(s);
-			whisper.remove(s);
-			gnarrator.add(s);
-		}
-		if (cmnd.getName().equalsIgnoreCase("ng") && strings.length >= 1) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			narrator(gnarrator_range, s, msg, gnarrator_str);
-		}
-		if (cmnd.getName().equalsIgnoreCase("me") && strings.length < 1) {
-			// s.sendMessage("Debug: whisper");
-			ic.remove(s);
-			shout.remove(s);
-			ooc.remove(s);
-			whisper.remove(s);
-			me.add(s);
-		}
-		if (cmnd.getName().equalsIgnoreCase("me") && strings.length >= 1) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			me(me_range, s, msg, me_str);
-	}*/	
 		return true;
 	}
 	@EventHandler
@@ -196,10 +78,6 @@ public class RpChat implements CommandExecutor, Listener {
 			chat.get(0).getList().add(p);
 		}
 		
-		/*if (!ooc.contains(p) && !ic.contains(p) && !shout.contains(p) && !whisper.contains(p) && !me.contains(p)
-		&& !narrator.contains(p) && !gnarrator.contains(p)) {
-			ic.add(p);
-		}*/
 		
 	for(int i =0 ; i<chat.size(); i++)
 	{
@@ -212,25 +90,6 @@ public class RpChat implements CommandExecutor, Listener {
 			narrator(chat.get(i).getRange(), p, s, chat.get(i).getPref());
 		}
 	}
-		/*
-		if (ic.contains(p)) {
-			message(ic_range, p, s, ic_str);
-		}
-		if (whisper.contains(p)) {
-			message(whisper_range, p, s, whisper_str);
-		}
-		if (shout.contains(p)) {
-			message(shout_range, p, s, shout_str);
-		}
-		if (me.contains(p)) {
-			me(me_range, p, s, me_str);
-		}
-		if (narrator.contains(p)) {
-			narrator(narrator_range, p, s, narrator_str);
-		}
-		if (gnarrator.contains(p)) {
-			narrator(gnarrator_range, p, s, gnarrator_str);
-		}*/
 		event.setCancelled(true);
 		
 	}
@@ -241,13 +100,8 @@ public class RpChat implements CommandExecutor, Listener {
 		int sprawdz = 0;
 		Location player_loc = p.getLocation();
 		for (int i = 0; i < rec.size(); i++) {
-			// p.sendMessage("test " +
-			// rec.get(i).getLocation().distance(player_loc));
 			if (rec.get(i).getLocation().distance(player_loc) < distance
 					&& rec.get(i).getLocation().distance(player_loc) > 0) {
-				// rec.get(i).sendMessage("test " +
-				// rec.get(i).getLocation().distance(player_loc));
-
 				rec.get(i).sendMessage(chatType + ChatColor.WHITE + p.getName() + ": " + s);
 				if (flaga2 == 0) {
 					p.sendMessage(chatType + ChatColor.WHITE + p.getName() + ": " + s);
@@ -262,7 +116,6 @@ public class RpChat implements CommandExecutor, Listener {
 		if (sprawdz == rec.size() - 1) {
 			p.sendMessage(chatType + ": " + ChatColor.RED + "[!] " + ChatColor.WHITE + "Nikt Cie nie slyszal"
 					+ ChatColor.RED + " [!]");
-			// p.sendMessage("flaga " + sprawdz);
 		}
 		flaga = 0;
 		flaga2 = 0;
@@ -276,12 +129,8 @@ public class RpChat implements CommandExecutor, Listener {
 		int sprawdz = 0;
 		Location player_loc = p.getLocation();
 		for (int i = 0; i < rec.size(); i++) {
-			// p.sendMessage("test " +
-			// rec.get(i).getLocation().distance(player_loc));
 			if (rec.get(i).getLocation().distance(player_loc) < distance
 					&& rec.get(i).getLocation().distance(player_loc) > 0) {
-				// rec.get(i).sendMessage("test " +
-				// rec.get(i).getLocation().distance(player_loc));
 
 				rec.get(i).sendMessage(chatType + p.getName() + " " + s);
 				if (flaga2 == 0) {
@@ -296,7 +145,6 @@ public class RpChat implements CommandExecutor, Listener {
 		}
 		if (sprawdz == rec.size() - 1) {
 			p.sendMessage(ChatColor.RED + "[!] " + chatType + "Nikt nie widzial co robisz" + ChatColor.RED + " [!]");
-			// p.sendMessage("flaga " + sprawdz);
 		}
 		flaga = 0;
 		flaga2 = 0;
@@ -310,12 +158,8 @@ public class RpChat implements CommandExecutor, Listener {
 		int sprawdz = 0;
 		Location player_loc = p.getLocation();
 		for (int i = 0; i < rec.size(); i++) {
-			// p.sendMessage("test " +
-			// rec.get(i).getLocation().distance(player_loc));
 			if (rec.get(i).getLocation().distance(player_loc) < distance
 					&& rec.get(i).getLocation().distance(player_loc) > 0) {
-				// rec.get(i).sendMessage("test " +
-				// rec.get(i).getLocation().distance(player_loc));
 
 				rec.get(i).sendMessage(chatType + ChatColor.WHITE + ": " + s);
 				if (flaga2 == 0) {
@@ -331,7 +175,6 @@ public class RpChat implements CommandExecutor, Listener {
 		if (sprawdz == rec.size() - 1) {
 			p.sendMessage(
 					ChatColor.RED + "[!] " + ChatColor.YELLOW + "Nikt nie widzial co robisz" + ChatColor.RED + " [!]");
-			// p.sendMessage("flaga " + sprawdz);
 		}
 		flaga = 0;
 		flaga2 = 0;
