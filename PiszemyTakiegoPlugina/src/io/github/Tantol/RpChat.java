3b5c8e4fe41e00a14d9eac5cddfdf8d365710840
DoneQ <marcinrubach@gmail.com>
2017-03-28 03:03:55 +0200

    Chat Poprawiony config

--- PiszemyTakiegoPlugina/src/io/github/Tantol/RpChat.java
+++ PiszemyTakiegoPlugina/src/io/github/Tantol/RpChat.java
@@ -13,20 +13,30 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class RpChat implements CommandExecutor, Listener {
 	private static ArrayList<ChatOb> chat = new ArrayList<ChatOb>();
 	private static ArrayList<Player> rec = new ArrayList<Player>();
-
+	private String msg_echo;
+	private String narrator_echo;
+	private String me_echo;
 	/*
 	 * Tutaj Poin¿ej Edytowaæ
 	 */
 
-	public RpChat(){
-		chat.add( new ChatOb("ic",30,ChatColor.GREEN + "[IC]", new ArrayList<Player>(),"msg"));
-		chat.add( new ChatOb("ooc",4000,ChatColor.RED + "[OOC]", new ArrayList<Player>(),"msg"));
-		chat.add( new ChatOb("w",5,ChatColor.GRAY + "[WHISPER]", new ArrayList<Player>(),"msg"));
-		chat.add( new ChatOb("s",60,ChatColor.BLUE + "[SHOUT]", new ArrayList<Player>(),"msg"));
-		chat.add( new ChatOb("me",30,ChatColor.YELLOW + "", new ArrayList<Player>(),"me"));
-		chat.add( new ChatOb("n",300,ChatColor.AQUA + "[NARRATOR]", new ArrayList<Player>(),"nar"));
-		chat.add( new ChatOb("ng",4000,ChatColor.AQUA + "[NARRATOR GLOBALNY]", new ArrayList<Player>(),"nar"));	
+	public RpChat() {
+		//Chaty, nie zapomnij dodaæ w mainie i w plugy.yml
+		chat.add(new ChatOb("ic", 30, ChatColor.GREEN + "[IC]", new ArrayList<Player>(), "msg"));
+		chat.add(new ChatOb("ooc", 4000, ChatColor.RED + "[OOC]", new ArrayList<Player>(), "msg"));
+		chat.add(new ChatOb("w", 5, ChatColor.GRAY + "[WHISPER]", new ArrayList<Player>(), "msg"));
+		chat.add(new ChatOb("s", 60, ChatColor.BLUE + "[SHOUT]", new ArrayList<Player>(), "msg"));
+		chat.add(new ChatOb("me", 30, ChatColor.YELLOW + "", new ArrayList<Player>(), "me"));
+		chat.add(new ChatOb("n", 300, ChatColor.AQUA + "[NARRATOR]", new ArrayList<Player>(), "nar"));
+		chat.add(new ChatOb("ng", 4000, ChatColor.AQUA + "[NARRATOR GLOBALNY]", new ArrayList<Player>(), "nar"));
+		
+		//Wiadomosc, ktora otrzymamy gdy nikogo nie bedzie w zasiegu chatu
+		msg_echo = " " + ChatColor.RED + "[!] " + ChatColor.WHITE + "Nikt Cie nie slyszal" + ChatColor.RED + " [!]";
+		narrator_echo = " " + ChatColor.RED + "[!] " + ChatColor.WHITE + "Nikt Cie nie slyszal" + ChatColor.RED + " [!]";
+		me_echo = ChatColor.RED + "[!] " + ChatColor.YELLOW + "Nikt nie widzial co robisz" + ChatColor.RED + " [!]";
+		
 	}
+
 	/*
 	 * Tego co pod tym lepiej nie tykaæ jak nie wiesz co i jak ^^
 	 */
@@ -38,60 +48,56 @@ public class RpChat implements CommandExecutor, Listener {
 		}
 
 		int flaga = 0;
-		for(int i =0 ; i<chat.size(); i++)
-		{
-		if (cmnd.getName().equalsIgnoreCase(chat.get(i).getCmd()) && strings.length >= 1 && flaga==0) {
-			rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
-			if(chat.get(i).getType()=="msg")
-			message(chat.get(i).getRange(), s, msg, chat.get(i).getPref());
-			if(chat.get(i).getType()=="me")
-				me(chat.get(i).getRange(), s, msg, chat.get(i).getPref());
-			if(chat.get(i).getType()=="nar")
-				narrator(chat.get(i).getRange(), s, msg, chat.get(i).getPref());
-			flaga=1;
-		}
-		if (cmnd.getName().equalsIgnoreCase(chat.get(i).getCmd()) && strings.length < 1) {
-			for(int j =0 ; j<chat.size(); j++)
-			{
-				chat.get(j).getList().remove(s);
+		for (int i = 0; i < chat.size(); i++) {
+			if (cmnd.getName().equalsIgnoreCase(chat.get(i).getCmd()) && strings.length >= 1 && flaga == 0) {
+				rec = new ArrayList<Player>(Dungeon.getPlugin().getServer().getOnlinePlayers());
+				if (chat.get(i).getType() == "msg")
+					message(chat.get(i).getRange(), s, msg, chat.get(i).getPref());
+				if (chat.get(i).getType() == "me")
+					me(chat.get(i).getRange(), s, msg, chat.get(i).getPref());
+				if (chat.get(i).getType() == "nar")
+					narrator(chat.get(i).getRange(), s, msg, chat.get(i).getPref());
+				flaga = 1;
 			}
-			chat.get(i).getList().add(s);
-			
+			if (cmnd.getName().equalsIgnoreCase(chat.get(i).getCmd()) && strings.length < 1) {
+				for (int j = 0; j < chat.size(); j++) {
+					chat.get(j).getList().remove(s);
+				}
+				chat.get(i).getList().add(s);
+
+			}
 		}
-		}
-		flaga=0;
+		flaga = 0;
 		return true;
 	}
+
 	@EventHandler
 	public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
 		Player p = event.getPlayer();
 		rec = new ArrayList<Player>(event.getRecipients());
 		String s = event.getMessage();
-		int flaga=0;
-		for(int i =0 ; i<chat.size(); i++)
-		{
-			if(!chat.get(i).getList().contains(p)){
+		int flaga = 0;
+		for (int i = 0; i < chat.size(); i++) {
+			if (!chat.get(i).getList().contains(p)) {
 				flaga++;
 			}
 		}
-		if(flaga==chat.size()){
+		if (flaga == chat.size()) {
 			chat.get(0).getList().add(p);
 		}
-		
-		
-	for(int i =0 ; i<chat.size(); i++)
-	{
-		if (chat.get(i).getList().contains(p)) {
-			if(chat.get(i).getType()=="msg")
-			message(chat.get(i).getRange(), p, s, chat.get(i).getPref());
-			if(chat.get(i).getType()=="me")
-		    me(chat.get(i).getRange(), p, s, chat.get(i).getPref());
-			if(chat.get(i).getType()=="nar")
-			narrator(chat.get(i).getRange(), p, s, chat.get(i).getPref());
+
+		for (int i = 0; i < chat.size(); i++) {
+			if (chat.get(i).getList().contains(p)) {
+				if (chat.get(i).getType() == "msg")
+					message(chat.get(i).getRange(), p, s, chat.get(i).getPref());
+				if (chat.get(i).getType() == "me")
+					me(chat.get(i).getRange(), p, s, chat.get(i).getPref());
+				if (chat.get(i).getType() == "nar")
+					narrator(chat.get(i).getRange(), p, s, chat.get(i).getPref());
+			}
 		}
-	}
 		event.setCancelled(true);
-		
+
 	}
 
 	public void message(int distance, Player p, String s, String chatType) {
@@ -114,8 +120,7 @@ public class RpChat implements CommandExecutor, Listener {
 			}
 		}
 		if (sprawdz == rec.size() - 1) {
-			p.sendMessage(chatType + ": " + ChatColor.RED + "[!] " + ChatColor.WHITE + "Nikt Cie nie slyszal"
-					+ ChatColor.RED + " [!]");
+			p.sendMessage(chatType + msg_echo);
 		}
 		flaga = 0;
 		flaga2 = 0;
@@ -144,7 +149,7 @@ public class RpChat implements CommandExecutor, Listener {
 			}
 		}
 		if (sprawdz == rec.size() - 1) {
-			p.sendMessage(ChatColor.RED + "[!] " + chatType + "Nikt nie widzial co robisz" + ChatColor.RED + " [!]");
+			p.sendMessage(me_echo);
 		}
 		flaga = 0;
 		flaga2 = 0;
@@ -174,7 +179,7 @@ public class RpChat implements CommandExecutor, Listener {
 		}
 		if (sprawdz == rec.size() - 1) {
 			p.sendMessage(
-					ChatColor.RED + "[!] " + ChatColor.YELLOW + "Nikt nie widzial co robisz" + ChatColor.RED + " [!]");
+					chatType + narrator_echo);
 		}
 		flaga = 0;
 		flaga2 = 0;
