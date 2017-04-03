f5e745f631de098ff8db821b36273ab091a3fc95
DoneQ <marcinrubach@gmail.com>
2017-04-04 01:39:08 +0200

    123

--- PiszemyTakiegoPlugina/src/io/github/Tantol/NewItems/ItemCommand.java
+++ PiszemyTakiegoPlugina/src/io/github/Tantol/NewItems/ItemCommand.java
@@ -25,15 +25,11 @@ public class ItemCommand implements CommandExecutor, Listener {
 
 	static ArrayList<CreateItem> items = new ArrayList<CreateItem>();
 	List<String> armor = Arrays.asList("ARMOR", "HELMET", "LEGS", "BOOTS");
-<<<<<<< HEAD
+
 	static int flaga_b = 0;
 	int taskId;
 
-=======
-	static int flaga_b = 0;
-	
 
->>>>>>> branch 'master' of https://github.com/Tantol/PiszemyTakiegoPlugina.git
 	public ItemCommand() {
 		if (flaga_b == 0) {
 			effects.add((ArrayList<String>) Dungeon.newItemsFlag.getCustomConfig().getList("ARMOR.players"));
