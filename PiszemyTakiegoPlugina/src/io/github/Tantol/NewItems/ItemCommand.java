ed3ba99d6a93196fd53a9b70d212a9fa0f0f8aa0
DoneQ <marcinrubach@gmail.com>
2017-04-04 01:35:48 +0200

    123

--- PiszemyTakiegoPlugina/src/io/github/Tantol/NewItems/ItemCommand.java
+++ PiszemyTakiegoPlugina/src/io/github/Tantol/NewItems/ItemCommand.java
@@ -26,7 +26,7 @@ public class ItemCommand implements CommandExecutor, Listener {
 	static ArrayList<CreateItem> items = new ArrayList<CreateItem>();
 	List<String> armor = Arrays.asList("ARMOR", "HELMET", "LEGS", "BOOTS");
 	static int flaga_b = 0;
-
+//
 	int taskId;
 
 	public ItemCommand() {
